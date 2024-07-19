812706cf44b4d74515c9067e16646db10aae4f40
itsratana <rithratana.hang@student.cadt.edu.kh>
2024-07-19 12:28:36 +0700

    profile screen

---
+++ lib/screens/profile_screen/profile_screen.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+class Profile_Screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Profile Screen',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+      ),
+      home: ProfileScreen(),
+    );
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height, 
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.green, Colors.black, Colors.black, Colors.black], 
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+          SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  AppBar(
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    iconTheme: IconThemeData(color: Colors.white),
+                    actions: [
+                      IconButton(
+                        icon: Icon(Icons.more_vert, color: Colors.white),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  CircleAvatar(
+                    radius: 50,
+                    backgroundImage: AssetImage('assets/images/profile_picture.jpg'), 
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Edit Profile'),
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black, backgroundColor: Colors.white, // Text color
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      _buildInfoColumn('18', 'Playlists'),
+                      SizedBox(width: 40),
+                      _buildInfoColumn('5', 'Followers'),
+                      SizedBox(width: 40),
+                      _buildInfoColumn('4', 'Following'),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  _buildPlaylists(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Column _buildInfoColumn(String count, String label) {
+    return Column(
+      children: [
+        Text(
+          count,
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        Text(
+          label,
+          style: TextStyle(color: Colors.white),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPlaylists() {
+    return Column(
+      children: [
+        _buildPlaylistItem('My Playlist #1','assets/images/playlist1.PNG', 7),
+        _buildPlaylistItem('My Playlist #2','assets/images/playlist2.PNG', 4),
+        _buildPlaylistItem('My Playlist #3','assets/images/playlist3.PNG', 5),
+        TextButton(
+          onPressed: () {},
+          child: Text('See all playlists', style: TextStyle(color: Colors.white)),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPlaylistItem(String title, String imagePath, int likes) {
+    return ListTile(
+      leading: Image.asset(imagePath, width: 40, height: 40),
+      title: Text(title, style: TextStyle(color: Colors.white)),
+      trailing: Text('$likes likes', style: TextStyle(color: Colors.white)),
+    );
+  }
+}
